a8c050f503b8d9ac8723e5ab4796baf9b2b82a58
EricCRodriguez <ecrod12@gmail.com>
2020-11-04 22:12:00 -0600

    began layout of menu page

--- src/views/ItemsIndex.vue
+++ src/views/ItemsIndex.vue
@@ -1,14 +1,34 @@
 <template>
-  <div class="container">
-    <h1>All Items</h1>
-    <div v-for="item in items">
-      <h2>{{ item.name }}</h2>
-      <p>Ingredients: {{ item.ingredients }}</p>
-      <p>Price: {{ item.price }}</p>
-      <router-link v-bind:to="`/items/${item.id}`">More details</router-link>
+  <div class="Menu">
+    <div class="container">
+      <h1>Menu</h1>
+      <section class="page-section clearfix"></section>
+
+      <section class="page-section cta">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-9 mx-auto">
+              <div class="cta-inner text-center rounded">
+                <h2 class="section-heading mb-4">
+                  <span class="section-heading-upper">El Cocina Fresca</span>
+                  <span class="section-heading-lower">Menu</span>
+                </h2>
+                <p class="mb-0"></p>
+                <div v-for="item in items">
+                  <h2>{{ item.name }}</h2>
+                  <p>Ingredients: {{ item.ingredients }}</p>
+                  <p>Price: {{ item.price }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
+
+<style></style>
 
 <script>
 import axios from "axios";
